906478d06c721da6b9ac2a44ffb79e54e5b1ea23
khizar121211 <khizarabbasshah5555@gmail.com>
2024-09-09 16:10:02 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923706111448"
+global.ownername = "🐛 HEER"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
